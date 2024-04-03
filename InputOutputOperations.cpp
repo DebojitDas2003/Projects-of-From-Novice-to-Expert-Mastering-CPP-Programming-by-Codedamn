eaeffd14680ba23d6e5465472b8f8e7423e370e0
DebojitDas2003 <debojitdas0110@gmail.com>
2024-04-04 01:25:51 +0530

    input & output

---
+++ InputOutputOperations.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    cout << "What is Your Name?: ";
+//cout statement with << operator is used to display information on the screen
+    cin >> name;
+//cin statement with >> operator is used to take input from the user
+    cout << "Fuck You " << name;
+    return 0;
+}
