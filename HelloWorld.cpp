ee7b339e4924af3c657df0d5696c15b24acb51cd
DebojitDas2003 <debojitdas0110@gmail.com>
2024-04-04 01:13:53 +0530

    Hello World

---
+++ HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+//#include is a preprocessor that tells the compiler which libraries to include before running the program
+using namespace std;
+
+
+int main() {
+//"main()" function is the starting point of the program
+    cout << "Hello World";
+//"cout" is a statement and "Hello World" is a expression, Statement and expression work together to make things work.
+    return 0;
+//"return" delivers the result of the program. If it's successful or not.
+}
